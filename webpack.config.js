a0809be887d51b2c0eff75d8a6be7054243146ba
Enclave88 <sutangu@gmail.com>
2016-07-29 15:49:45 +0600

    another localhost

--- webpack.config.js
+++ webpack.config.js
@@ -35,12 +35,12 @@ module.exports = {
     output: {
         path: './public/built',
         filename: 'bundle.js',
-        publicPath: 'http://localhost:8080/built/'
+        publicPath: 'http://127.0.0.2:8080/built/'
     },
 
     devServer: {
         contentBase: './public',
-        publicPath: 'http://localhost:8080/built/'
+        publicPath: 'http://127.0.0.2:8080/built/'
     },
 
     module: {
