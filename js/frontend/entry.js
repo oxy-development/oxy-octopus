2dc813ef94126ddcbc70e7097b79d1170490dec6
ygintsyak <gintsyak@gmail.com>
2016-08-01 22:40:24 +0600

    Working on project structure

---
+++ js/frontend/entry.js
@@ -0,0 +1,14 @@
+require('../less/main.less');
+
+'use strict';
+
+
+import React from "react";
+import ReactDOM from 'react-dom';
+
+import ObjectPage from './object-page.js';
+
+let Component = React.createElement;
+
+//ReactDOM.render(<div className="myDiv">Hello React + Electron</div>, document.querySelector('body'));
+ReactDOM.render(Component(ObjectPage), document.querySelector('body'));
--- main.js
+++ main.js
@@ -36,6 +36,13 @@ app.on('window-all-closed', function() {
 // Этот метод будет вызван когда Electron закончит инициализацию
 // и будет готов к созданию браузерных окон.
 app.on('ready', function() {
+    
+    // import ProjectInstanceStub from "js/backend/backend_entry.js";
+    'use strict';
+    var Project = require('./js/backend/backend_entry.js');
+    new Project().printHello();
+    // new ProjectInstanceStub('./main.js').printHello();
+    
     // Создаем окно браузера.
     mainWindow = new BrowserWindow({
         width: 800,
